c6e62ee264507c879a79358e2a52c3cf647b7c9d
choiway <waynechoi@gmail.com>
2018-08-15 11:45:34 -0700

    .gitignore fix

--- .elixir_ls/build/test/lib/gradual_types/ebin/gradual_types.app
+++
@@ -1,7 +0,0 @@
-{application,gradual_types,
-             [{applications,[kernel,stdlib,elixir,logger]},
-              {description,"gradual_types"},
-              {modules,['Elixir.GradualTypes','Elixir.Login']},
-              {registered,[]},
-              {vsn,"0.1.0"},
-              {extra_applications,[logger]}]}.
--- lib/login.ex
+++ lib/login.ex
@@ -2,10 +2,13 @@ defmodule Login do
   @moduledoc """
 
   """
+  defstruct username: nil,  age: nil, email_address: nil
 
   def print(params) do
     next_year_age = params["age"] + 1
 
-    "username: #{params["username"]}, email_address: #{params["email_address"]}, next_year_age: #{next_year_age}"
+    "username: #{params["username"]}, email_address: #{params["email_address"]}, next_year_age: #{
+      next_year_age
+    }"
   end
 end
