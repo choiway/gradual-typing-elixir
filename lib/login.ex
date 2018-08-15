a09f0b7fd5f51ec061f7bfe11e887e53330b393e
choiway <waynechoi@gmail.com>
2018-08-15 11:19:43 -0700

    Add login with params and tests

---
+++ lib/login.ex
@@ -0,0 +1,10 @@
+defmodule Login do
+  @moduledoc """
+
+  """
+
+  def print(params) do
+    next_year_age = params["age"] + 1
+    "username: #{params["username"]}, email_address: #{params["email_address"]}, next_year_age: #{next_year_age}"
+  end
+end
